f1426c61eea8db54f2ad42225d8e049ec3af5db4
elias <elias.jm.garcia@gmail.com>
2018-04-13 15:38:19 -0500

    attempt db implementation again with correct user fields

--- tabularius/models.py
+++ tabularius/models.py
@@ -4,7 +4,7 @@ from tabularius import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), )
+    email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
     def __repr__(self):
