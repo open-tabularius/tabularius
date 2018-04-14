40b06ea1e7b24aa2f70e6f34084bc7bb85923e88
elias <elias.jm.garcia@gmail.com>
2018-04-14 18:07:42 -0500

    beginning user login handling

--- tabularius/__init__.py
+++ tabularius/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 # application is created as an instance of Flask and we pass it __name__
 # in order load resources correctly for our module
@@ -11,6 +12,9 @@ app.config.from_object(Config)
 # database setup
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+# handle logins
+login = LoginManager(app)
 
 # we place the import statement here to avoid circular imports.
 from tabularius import routes, models
--- tabularius/models.py
+++ tabularius/models.py
@@ -1,13 +1,21 @@
 from tabularius import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     documents = db.relationship('Document', backref='author', lazy='dynamic')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -33,3 +41,6 @@ class Student(db.Model):
 
     def __repr__(self):
         return '<Student {}>'.format(self.ssn_id)
+
+
+# class Attendance(db.
