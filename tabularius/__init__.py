8f850fd135416389d5134f1e0f515c0c45d81438
elias <elias.jm.garcia@gmail.com>
2018-04-15 18:07:12 -0500

    enabled logging to file

--- tabularius/__init__.py
+++ tabularius/__init__.py
@@ -4,7 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 import logging
-from logging.handlers import SMTPHandler
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 
 # application is created as an instance of Flask and we pass it __name__
 # in order load resources correctly for our module
@@ -41,6 +42,17 @@ if not app.debug:
                     secure=secure)
                 mail_handler.setLevel(logging.ERROR)
                 app.logger.addHandler(mail_handler)
+            if not os.path.exists('logs'):
+                os.mkdir('logs')
+                file_handler = RotatingFileHandler(
+                    'logs/tabularius.log', maxBytes=10240, backupCount=10)
+                file_handler.setFormatter(
+                    logging.Formatter('%(asctime)s %(levelname)s: %(message)s '
+                                      + '[in %(pathname)s:%(lineno)d]'))
+                file_handler.setLevel(logging.INFO)
+                app.logger.addHandler(file_handler)
+                app.logger.setLevel(logging.INFO)
+                app.logger.info('tabularius startup')
 
 # we place the import statement here to avoid circular imports.
 from tabularius import routes, models, errors
