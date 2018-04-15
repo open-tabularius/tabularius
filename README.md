f5bfe5139323c660220cd71252f6100560f93db6
elias <elias.jm.garcia@gmail.com>
2018-04-15 17:42:57 -0500

    enabled email error logging!

--- README.md
+++ README.md
@@ -23,3 +23,18 @@ To enable debugging, also do the following:
 ```bash
 export FLASK_DEBUG=1
 ```
+
+To configure the email server, you need to add your email into the admin field in `config.py`.
+
+Then, export the following:
+```bash
+exportexportexportMAIL_SERVER=smtp.googlemail.com
+MAIL_PORT=587
+MAIL_USE_TLS=1
+exportexportMAIL_USERNAME=<your-gmail-username>
+MAIL_PASSWORD=<your-gmail-password>
+```
+
+Where you replace the values in `<>` with the desired email address. Please,
+for all that is good, do not upload these credentials onto a repository
+anywhere online.
--- tabularius/__init__.py
+++ tabularius/__init__.py
@@ -3,7 +3,8 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-from flask_avatar import Avatar
+import logging
+from logging.handlers import SMTPHandler
 
 # application is created as an instance of Flask and we pass it __name__
 # in order load resources correctly for our module
@@ -22,5 +23,24 @@ login.login_view = 'login'
 # handle avatars
 # avatar = Avatar(app)
 
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+            secure = None
+            if app.config['MAIL_USE_TLS']:
+                secure = ()
+                mail_handler = SMTPHandler(
+                    mailhost=(app.config['MAIL_SERVER'],
+                              app.config['MAIL_PORT']),
+                    fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+                    toaddrs=app.config['ADMINS'],
+                    subject='tabularius failure',
+                    credentials=auth,
+                    secure=secure)
+                mail_handler.setLevel(logging.ERROR)
+                app.logger.addHandler(mail_handler)
+
 # we place the import statement here to avoid circular imports.
-from tabularius import routes, models
+from tabularius import routes, models, errors
