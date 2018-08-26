df5c19dac4d95202c700129db75e963c2650e9cd
elias <elias.jm.garcia@gmail.com>
2018-08-26 17:17:18 -0500

    added basic dbs support

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__name__))
+DATABASE_URI = "postgresql://spook:tab_dev@haunt/tab_dev"
+
 
 class Config():
+    SQLALCHEMY_DATABASE_URI = DATABASE_URI
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or "wu tang clan comin' at ya"
--- tab/__init__.py
+++ tab/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 tab_app = Flask(__name__)
 tab_app.config.from_object(Config)
 
+# DB stuff
+db = SQLAlchemy(tab_app)
+migrate = Migrate(tab_app, db)
+
 # avoids circular dependencies because python is special like that
-from tab import routes
+from tab import routes, models
